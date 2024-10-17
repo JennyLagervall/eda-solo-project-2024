f45de447df6cd6f572d2110017cb50ed0d0cafd6
JennyLagervall <156864102+JennyLagervall@users.noreply.github.com>
2024-10-17 12:44:00 -0500

    support redux devtools

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,4 +1,4 @@
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import createSagaMiddleware from 'redux-saga';
 import logger from 'redux-logger';
 
@@ -12,12 +12,13 @@ const sagaMiddleware = createSagaMiddleware();
 // logger will only be added to your project if your in development mode
 const middlewareList = process.env.NODE_ENV === 'development' ? [sagaMiddleware, logger] : [sagaMiddleware];
 
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(
   // tells the saga middleware to use the rootReducer
   // rootSaga contains all of our other reducers
   rootReducer,
   // adds all middleware to our project including saga and logger
-  applyMiddleware(...middlewareList)
+  composeEnhancers(applyMiddleware(...middlewareList))
 );
 
 // tells the saga middleware to use the rootSaga
